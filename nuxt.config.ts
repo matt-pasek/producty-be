883ccb2c1574a721099ce31dbdd79ec7668d5e69
matt-pasek <m_pasek@icloud.com>
2025-04-01 21:11:09 +0200

    prettier and pinia installation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/image'],
+  modules: ['@nuxt/eslint', '@nuxt/image', '@pinia/nuxt'],
   css: ['~/assets/css/main.css'],
   vite: {
     plugins: [
